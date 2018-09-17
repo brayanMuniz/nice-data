80005b45700332da38462cd588880d3048a69cff
Brayan Muniz <brayanmuniz2636@gmail.com>
2018-09-17 15:51:43 -0500

    addded props to dashboard

--- src/components/dashboardFiles/dashboard.js
+++ src/components/dashboardFiles/dashboard.js
@@ -1,10 +1,13 @@
 // Todo https://apexcharts.com/javascript-chart-demos/mixed-charts/line-column-area/ 
 export default {
   name: "dashboard",
-  data: function() {
+  data(){
     return {
 
     };
   },
+  components: {
 
+  },
+  props: ['userNHAddressData', 'currentBITPriceNum']
 };
