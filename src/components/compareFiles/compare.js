615392b230343137b0943f02230528319c96a38a
Brayan Muniz <brayanmuniz2636@gmail.com>
2018-09-19 07:50:52 -0500

    Example of how profit works

--- src/components/compareFiles/compare.js
+++ src/components/compareFiles/compare.js
@@ -1,4 +1,5 @@
 /*eslint-disable*/
+import axios from 'axios'
 import lineChart from '../Charts/lineChartFiles/lineChart.vue'
 export default {
     name: "compare",
@@ -13,13 +14,35 @@ export default {
             this.userData = {
                 labels: ["kek", 'topKek'],
                 datasets: [{
-                    label: '',
+                    label: 'Addr 1',
                     backgroundColor: '#f87979',
-                    data: [(Math.floor(Math.random() * (50 - 5 + 1)) + 5), 3]
+                    data: [(Math.floor(Math.random() * (50 - 5 + 1)) + 5), (Math.floor(Math.random() * (50 - 5 + 1)) + 5)]
                 }]
             }
-            console.log(this.userData)
-        }
+        },
+        testAddress() {
+            axios.get('api/nh', {
+                params: {
+                    method: 'stats.provider.ex',
+                    addr: "3Ls4oRPWP3rxhPKsWgi4CYaK6E6c8HSekv"
+                }
+            }).
+            then(res => {
+                console.log(res.data)
+                let totalCurrentProfit = 0
+                for (let profit in res.data.result.current) {
+                    totalCurrentProfit += Number(res.data.result.current[profit].profitability)
+                }
+
+                console.log(totalCurrentProfit)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+    },
+    created() {
+        this.testAddress()
     },
     components: {
         "line-chart": lineChart
