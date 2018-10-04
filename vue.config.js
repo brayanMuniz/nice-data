8c236f376775aa1ca7ae925d35179c8367400cfc
Brayan Muniz <brayanmuniz2636@gmail.com>
2018-10-04 13:43:19 -0500

    proxy test

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    // baseUrl: process.env.NODE_ENV === 'production' ?
-    //     '/production-sub-path/' : '/',
+    baseUrl: process.env.NODE_ENV === 'production' ?
+        '/production-sub-path/' : '/',
+    // Todo Change the proxy to the actual deployment base
+
     devServer: {
         proxy: {
             '/api': {
                 target: 'https://api.nicehash.com',
-                ws: true,
                 changeOrigin: true,
             }
         }
