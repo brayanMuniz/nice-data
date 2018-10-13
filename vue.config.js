294892b618aff3c9fac2763de735919319b034ea
Brayan Muniz <brayanmuniz2636@gmail.com>
2018-10-13 08:56:24 -0500

    proxy test 6 -removed value in package.json and configure webpakc simple

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,14 @@
 module.exports = {
-    // baseUrl: process.env.NODE_ENV === 'production' ?
-    //     '/production-sub-path/' : '/',
-    // Todo Change the proxy to the actual deployment base
-    // ? It does not work becasue at deployment it does not know what the dev server is 
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'https://api.nicehash.com',
-                changeOrigin: true,
-            }
-        }
-    },
-    configureWebpack: config => {
-        proxy = {
-            '/api': {
-                target: 'https://api.nicehash.com',
-                changeOrigin: true,
-            }
-        }
-        if (process.env.NODE_ENV === 'production') {
-            config.devServer = {
-                proxy: {
-                    '/api': {
-                        target: 'https://api.nicehash.com',
-                        changeOrigin: true,
-                    }
-                }
-            }
-
-        } else {
-            config.devServer = {
-                proxy: {
-                    '/api': {
-                        target: 'https://api.nicehash.com',
-                        changeOrigin: true,
-                    }
+    configureWebpack: {
+        devServer: {
+            proxy: {
+                '/api': {
+                    target: 'https://api.nicehash.com',
+                    changeOrigin: true,
                 }
             }
         }
     }
+
+
 }
